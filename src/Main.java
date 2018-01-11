1b8bb5a9e265ed41dba5056946df0eefd83bd1b2
liamjg <gallagherl0491@my.uwstout.edu>
2018-01-10 19:43:25 -0600

    Made type generic

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,13 @@ public class Main {
 
         System.out.println("List is empty: " + test.isEmpty());
 
+        test.addLast(1);
+        test.addLast(2);
+        test.addLast(3);
         test.addLast('a');
-        test.addLast('b');
-        test.addLast('c');
-        test.addLast('d');
-        test.addLast('e');
-        
+        test.addLast("bcd");
+        test.addFirst(0);
+
 
         test.printList();
 
